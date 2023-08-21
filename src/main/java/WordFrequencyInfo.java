ee4b198eb54a0524dc4250325b84e50c0b97464f
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-21 18:36:28 +0800

    refactor: replaced names in WordFrequencyInfo class with more meaningful names

---
+++ src/main/java/WordFrequencyInfo.java
@@ -0,0 +1,18 @@
+public class WordFrequencyInfo {
+    private final String word;
+    private final int count;
+
+    public WordFrequencyInfo(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.count;
+    }
+
+}
