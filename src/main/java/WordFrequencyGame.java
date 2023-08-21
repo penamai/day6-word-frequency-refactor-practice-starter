81859d307c47b49e763b70b67c1d9a4a0b563f33
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-21 20:11:10 +0800

    refactor: extracted methods from getWordsWithFrequencies

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,39 +3,51 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public static final String MULTIPLE_SPACES_DELIMITER = "\\s+";
-    public static final String NEWLINE_DELIMITER = "\n";
-    public static final int ONE = 1;
     public static final String CALCULATE_ERROR = "Calculate Error";
+    public static final String NEWLINE_DELIMITER = "\n";
 
     public String getWordsWithFrequencies(String inputString) {
         try {
-            //split the input string with 1 to n pieces of spaces
-            List<String> wordsArray = List.of(inputString.split(MULTIPLE_SPACES_DELIMITER));
-
-            List<WordFrequencyInfo> wordFrequencyInfoList = wordsArray.stream()
-                    .map(word -> new WordFrequencyInfo(word, ONE))
-                    .collect(Collectors.toList());
-
-            //get the map for the next step of sizing the same word
+            List<String> wordsArray = getListOfWords(inputString);
+            List<WordFrequencyInfo> wordFrequencyInfoList = convertToWordFrequencyInfoList(wordsArray);
             Map<String, List<WordFrequencyInfo>> stringWordFrequencyInfoMap = getListMap(wordFrequencyInfoList);
 
-            List<WordFrequencyInfo> wordFrequencyInfos = stringWordFrequencyInfoMap.entrySet().stream()
-                    .map(entry -> new WordFrequencyInfo(entry.getKey(), entry.getValue().size()))
-                    .collect(Collectors.toList());
+            List<WordFrequencyInfo> wordFrequencyInfos = convertToWordFrequencyInfoList(stringWordFrequencyInfoMap);
 
             wordFrequencyInfoList = wordFrequencyInfos;
 
             wordFrequencyInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
-            StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
-            wordFrequencyInfoList.stream()
-                    .map(wordFrequencyInfo -> wordFrequencyInfo.getWord() + " " + wordFrequencyInfo.getWordCount())
-                    .forEach(joiner::add);
-
-            return joiner.toString();
+            return generatePrintLines(wordFrequencyInfoList);
         } catch (Exception exception) {
             return CALCULATE_ERROR;
         }
+    }
+
+    private static List<String> getListOfWords(String inputString) {
+        return List.of(inputString.split(MULTIPLE_SPACES_DELIMITER));
+    }
+
+    private static List<WordFrequencyInfo> convertToWordFrequencyInfoList(List<String> wordsArray) {
+        return wordsArray.stream()
+                .map(WordFrequencyInfo::new)
+                .collect(Collectors.toList());
+    }
+
+    private static List<WordFrequencyInfo> convertToWordFrequencyInfoList(Map<String, List<WordFrequencyInfo>> stringWordFrequencyInfoMap) {
+        return stringWordFrequencyInfoMap.entrySet().stream()
+                .map(WordFrequencyInfo::new)
+                .collect(Collectors.toList());
+    }
+
+    private static String generatePrintLines(List<WordFrequencyInfo> wordFrequencyInfoList) {
+        return wordFrequencyInfoList.stream()
+                .map(WordFrequencyGame::generateLineContent)
+                .collect(Collectors.joining(NEWLINE_DELIMITER));
+    }
+
+    private static String generateLineContent(WordFrequencyInfo wordFrequencyInfo) {
+        return wordFrequencyInfo.getWord() + " " + wordFrequencyInfo.getWordCount();
     }
 
     private Map<String, List<WordFrequencyInfo>> getListMap(List<WordFrequencyInfo> wordFrequencyInfoList) {
--- src/main/java/WordFrequencyInfo.java
+++ src/main/java/WordFrequencyInfo.java
@@ -1,10 +1,24 @@
+import java.util.List;
+import java.util.Map;
+
 public class WordFrequencyInfo {
+    public static final int DEFAULT_COUNT = 1;
     private final String word;
     private final int count;
 
     public WordFrequencyInfo(String word, int count) {
         this.word = word;
         this.count = count;
+    }
+
+    public WordFrequencyInfo(String word) {
+        this.word = word;
+        this.count = DEFAULT_COUNT;
+    }
+
+    public WordFrequencyInfo(Map.Entry<String, List<WordFrequencyInfo>> entry) {
+        this.word = entry.getKey();
+        this.count = entry.getValue().size();
     }
 
     public String getWord() {
