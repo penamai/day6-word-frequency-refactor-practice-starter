aec78e9c2084b4c36f4cd62e10b6b3f60e0d3a31
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-21 19:21:37 +0800

    refactor: replaced for loop with stream API

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,8 +1,8 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-    public static final String SPACE_DELIMITER = "\\s";
-    public static final String PLUS_SIGN = "+";
+    public static final String MULTIPLE_SPACES_DELIMITER = "\\s+";
     public static final String NEWLINE_DELIMITER = "\n";
     public static final int ONE = 1;
     public static final String CALCULATE_ERROR = "Calculate Error";
@@ -10,31 +10,28 @@ public class WordFrequencyGame {
     public String getWordsWithFrequencies(String inputString) {
         try {
             //split the input string with 1 to n pieces of spaces
-            String[] wordsArray = inputString.split(SPACE_DELIMITER + PLUS_SIGN);
+            List<String> wordsArray = List.of(inputString.split(MULTIPLE_SPACES_DELIMITER));
 
-            List<WordFrequencyInfo> wordFrequencyInfoList = new ArrayList<>();
-            for (String word : wordsArray) {
-                WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(word, ONE);
-                wordFrequencyInfoList.add(wordFrequencyInfo);
-            }
+            List<WordFrequencyInfo> wordFrequencyInfoList = wordsArray.stream()
+                    .map(word -> new WordFrequencyInfo(word, ONE))
+                    .collect(Collectors.toList());
 
             //get the map for the next step of sizing the same word
             Map<String, List<WordFrequencyInfo>> stringWordFrequencyInfoMap = getListMap(wordFrequencyInfoList);
 
-            List<WordFrequencyInfo> wordFrequencyInfos = new ArrayList<>();
-            for (Map.Entry<String, List<WordFrequencyInfo>> entry : stringWordFrequencyInfoMap.entrySet()) {
-                WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(entry.getKey(), entry.getValue().size());
-                wordFrequencyInfos.add(wordFrequencyInfo);
-            }
+            List<WordFrequencyInfo> wordFrequencyInfos = stringWordFrequencyInfoMap.entrySet().stream()
+                    .map(entry -> new WordFrequencyInfo(entry.getKey(), entry.getValue().size()))
+                    .collect(Collectors.toList());
+
             wordFrequencyInfoList = wordFrequencyInfos;
 
             wordFrequencyInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
             StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
-            for (WordFrequencyInfo wordFrequencyInfo : wordFrequencyInfoList) {
-                String s = wordFrequencyInfo.getWord() + " " + wordFrequencyInfo.getWordCount();
-                joiner.add(s);
-            }
+            wordFrequencyInfoList.stream()
+                    .map(wordFrequencyInfo -> wordFrequencyInfo.getWord() + " " + wordFrequencyInfo.getWordCount())
+                    .forEach(joiner::add);
+
             return joiner.toString();
         } catch (Exception exception) {
             return CALCULATE_ERROR;
@@ -43,8 +40,7 @@ public class WordFrequencyGame {
 
     private Map<String, List<WordFrequencyInfo>> getListMap(List<WordFrequencyInfo> wordFrequencyInfoList) {
         Map<String, List<WordFrequencyInfo>> map = new HashMap<>();
-        for (WordFrequencyInfo wordFrequencyInfo : wordFrequencyInfoList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
+        wordFrequencyInfoList.forEach(wordFrequencyInfo -> {
             if (!map.containsKey(wordFrequencyInfo.getWord())) {
                 ArrayList<WordFrequencyInfo> arr = new ArrayList<>();
                 arr.add(wordFrequencyInfo);
@@ -52,7 +48,7 @@ public class WordFrequencyGame {
             } else {
                 map.get(wordFrequencyInfo.getWord()).add(wordFrequencyInfo);
             }
-        }
+        });
         return map;
     }
 
